679569b4d2f2f25172ad841487524c466d48c3a5
rxpfkb <174620163+rxpfkb@users.noreply.github.com>
2026-08-31 12:58:34 -0300

    Transforma o site em PWA instalavel: icones, manifest e service worker

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "rp-site-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./verificar-audio.html",
+  "./assets/css/style.css",
+  "./assets/js/audio-check.js",
+  "./manifest.webmanifest",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png",
+  "./assets/icons/favicon.svg",
+  "./assets/icons/apple-touch-icon.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) { return cache.addAll(APP_SHELL); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE_NAME; }).map(function (k) { return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  var req = event.request;
+  if (req.method !== "GET" || new URL(req.url).origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(req).then(function (cached) {
+      var network = fetch(req).then(function (res) {
+        if (res && res.ok) {
+          var copy = res.clone();
+          caches.open(CACHE_NAME).then(function (cache) { cache.put(req, copy); });
+        }
+        return res;
+      }).catch(function () { return cached; });
+      return cached || network;
+    })
+  );
+});
